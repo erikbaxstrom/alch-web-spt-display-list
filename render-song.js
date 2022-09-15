b484c2f441436a8d4db82a9fcfa3da639de63f27
Erik M Baxstrom <baxstrem@gmail.com>
2022-09-15 12:55:43 -0700

    fix song class attribute

--- render-song.js
+++ render-song.js
@@ -2,6 +2,6 @@ export function renderSong(song) {
     console.log(song);
     const li = document.createElement('li');
     li.textContent = song;
-    li.classList.add('song-list');
+    li.classList.add('song-item');
     return li;
 }
