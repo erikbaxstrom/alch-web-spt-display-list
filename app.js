22d529eb7eac618d5fb17ee761177763b07f0d69
Erik M Baxstrom <baxstrem@gmail.com>
2022-09-15 12:35:15 -0700

    render and display list of songs

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 // import functions and grab DOM elements
+import { mySongs } from './songs-data.js';
+import { renderSong } from './render-song.js';
+
+const songList = document.getElementById('song-list');
 
 // initialize global state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
+
+function displaySongs() {
+    for (let song of mySongs) {
+        let songEl = renderSong(song);
+        songList.append(songEl);
+    }
+}
+
+displaySongs();
---
+++ songs-data.js
@@ -0,0 +1,1 @@
+export const mySongs = ['Cantaloupe Island', '7 Rings', 'Nights in White Satin'];
